ba2adfada4144cc9eafd34ed48631a44637dd5d4
argadia <argadiabagus06@gmail.comgit config --global user.email argadiabagus06@gmail.comgit initgit add .git commit -m first>
2023-03-05 23:58:29 +0700

    first commit

---
+++ js/embla.js
@@ -0,0 +1,11 @@
+// import EmblaCarousel from 'embla-carousel'
+// import Autoplay from 'embla-carousel-autoplay'
+
+// const autoplayOptions = {
+//   delay: 4000,
+//   rootNode: (emblaRoot) => emblaRoot.parentElement,
+// }
+
+// const embla = EmblaCarousel(emblaRoot, { loop: false }, [
+//   Autoplay(autoplayOptions), // Add plugin with options
+// ])
