4fb9d54d07d4cbc95227ad27dae6ceda1f9c6980
best4youu <argadiabagus06@gmail.comgit config --global user.email argadiabagus06@gmail.comgit initgit add .git commit -m first>
2023-03-08 01:32:20 +0700

    first commit

--- owlCarousel/owl.carousel.js
+++ owlCarousel/owl.carousel.js
@@ -3446,3 +3446,21 @@
 	}
 
 })(window.Zepto || window.jQuery, window, document);
+
+$('.owl-carousel').owlCarousel({
+    stagePadding: 50,
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
